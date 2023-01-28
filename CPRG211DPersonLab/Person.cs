54a3edf4e43d397620179845207f847a8e1032cd
Nick Hamnett <nick.hamnett@sait.ca>
2023-01-28 11:40:29 -0700

    Completed lab solution
    Added comments
    Creates Relation class
    Creates Persons
    Solution file is included

---
+++ CPRG211DPersonLab/Person.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPRG211DPersonLab
+{
+    /// <summary>
+    /// Represents Person
+    /// </summary>
+    /// <remarks>Author: Nick Hamnett</remarks>
+    /// <remarks>Date: Jan 27, 2023</remarks>
+    internal class Person
+    {
+        // Fields
+        private int id;
+        private string firstName;
+        private string lastName;
+        private int age;
+        private string favoriteColor;
+        private bool isWorking;
+
+        // Properties
+        public int Id { get => id; set => id = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                // Ensure age is valid
+                if (value >= 0 && value <= 122)
+                {
+                    age = value;
+                }
+            }
+        }
+        public string FavoriteColor { get => favoriteColor; set => favoriteColor = value; }
+        public bool IsWorking { get => isWorking; set => isWorking = value; }
+
+        /// <summary>
+        /// User-defined constructor for Person
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <param name="age"></param>
+        /// <param name="favoriteColor"></param>
+        /// <param name="isWorking"></param>
+        public Person(int id, string firstName, string lastName, int age, string favoriteColor, bool isWorking)
+        {
+            this.id = id;
+            this.firstName = firstName;
+            this.lastName = lastName;
+            Age = age;
+            FavoriteColor = favoriteColor;
+            IsWorking = isWorking;
+        }
+
+        /// <summary>
+        /// Generates string with current persons info
+        /// </summary>
+        /// <returns>Formatted string</returns>
+        public string DisplayPersonInfo()
+        {
+            string formatted = "";
+
+            formatted += "ID: " + id + "\n";
+            formatted += "First name: " + firstName + "\n";
+            formatted += "Last name: " + lastName + "\n";
+            //formatted += "Age: " + age + "\n";
+            formatted += "Favorite Color: " + favoriteColor + "\n";
+            //formatted += "Is Working: " + isWorking + "\n";
+
+            //formatted = string.Format("ID: {0}\nFirst name: {1}", id, firstName);
+
+            //formatted = $"ID: {id}\n";
+
+            return formatted;
+        }
+
+        /// <summary>
+        /// Changes persons favorite color to White
+        /// </summary>
+        public void ChangeFavoriteColor()
+        {
+            favoriteColor = "White";
+        }
+
+        //public static void ChangeFavoriteColor(Person p)
+        //{
+        //    p.favoriteColor = "White";
+        //}
+
+        /// <summary>
+        /// Gets current person age in 10 years
+        /// </summary>
+        /// <returns>Age in 10 years</returns>
+        public int GetAgeInTenYears()
+        {
+            int ageInTenYears = age + 10;
+
+            return ageInTenYears;
+        }
+
+        /// <summary>
+        /// Gets Person as human readable string
+        /// </summary>
+        /// <returns>Formatted string</returns>
+        public override string ToString()
+        {
+            string formatted = "";
+
+            formatted += "ID\t\t" + id + "\n";
+            formatted += "First name\t" + firstName + "\n";
+            formatted += "Last name\t" + lastName + "\n" ;
+
+            return formatted;
+        }
+    }
+}
---
+++ CPRG211DPersonLab/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPRG211DPersonLab
+{
+    /// <summary>
+    /// CPRG211: Lab 1 (Classes and Objects)
+    /// </summary>
+    /// <remarks>Author: Nick Hamnett</remarks>
+    /// <remarks>Date: Jan 27, 2023</remarks>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Person objects
+            Person person1 = new Person(1, "Ian", "Brooks", 30, "Red", true);
+            Person person2 = new Person(2, "Gina", "Smith", 18, "Red", false);
+            Person person3 = new Person(3, "Mike", "Briscoe", 45, "Blue", true);
+            Person person4 = new Person(4, "Mary", "Beeals", 28, "Yellow", true);
+
+            // Display Gina's information
+            string ginasInfo = person2.DisplayPersonInfo();
+            Console.WriteLine(ginasInfo);
+
+            // Display Mike's information
+            string mikesInfo = person3.ToString();
+            Console.WriteLine(mikesInfo);
+
+            // Change Ian's favorite color to white
+            person1.ChangeFavoriteColor();
+            //Person.ChangeFavoriteColor(person1);
+
+            // Display Mary's age in 10 years
+            int marysAgeTenYears = person4.GetAgeInTenYears();
+            Console.WriteLine("Mary's age in 10 years: " + marysAgeTenYears);
+
+            // Create Relation instances (linking Person instances together)
+            Relation relation1 = new Relation("Sister", person2, "Sister", person4);
+            Relation relation2 = new Relation("Brother", person1, "Brother", person3);
+
+            // Display first relation
+            Console.WriteLine(relation1.ToString());
+
+            // Create and populate list of people
+            List<Person> people = new List<Person>();
+
+            people.Add(person1);
+            people.Add(person2);
+            people.Add(person3);
+            people.Add(person4);
+
+            // Determine average age
+            int sum = 0;
+
+            // Get sum of everyone's age
+            foreach (Person person in people)
+            {
+                sum += person.Age;
+            }
+
+            // Divide sum by number of people.
+            // One operand for / operator must be double for result to be double.
+            double average = sum / (double) people.Count;
+
+            // Display average age
+            Console.WriteLine("Average age: " + average);
+        }
+    }
+}
